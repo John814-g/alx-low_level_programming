2955933ef114d359e84db1e2f2b299f471848590
john814-g <yewhans2021@gamil.com>
2023-02-21 17:12:54 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+        _putchar("char" _);
+        _putchar("char" p);
+	_putchar("char" u);
+        _putchar("char" t);
+        _putchar("char" c);
+        _putchar("char" h);
+	_putchar("char"  a);
+	_putchar("char" r);	
+        _putchar("\n");
+
+
+        return (0);
+
+}	
