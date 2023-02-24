f30d03d1a24441ee7acbaea38ddd64c33340f144
john814-g <yewhans2021@gamil.com>
2023-02-24 10:05:48 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
+
+/**
+ * _isdigit - Checks if a character is a digit
+ * @c: The character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
